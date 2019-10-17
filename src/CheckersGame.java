9fd449e5beb090b908bc87507fdffad3a422723f
Sam Henkes - Desktop <shenkes101@yahoo.com>
2019-10-17 15:22:57 -0500

    added input to decide choose who is playing

---
+++ src/CheckersGame.java
@@ -0,0 +1,14 @@
+public class CheckersGame {
+
+    private int player1, player2;
+
+    public CheckersGame(int player1, int player2){
+        this.player1 = player1;
+        this.player2 = player2;
+    }
+
+    public void Start(boolean player1First){
+
+    }
+
+}
--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,38 @@
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+        int player1 = -1, player2 = -1;
+
+        System.out.println("Choose a difficulty for Player 1.\nType 0 for a human player.\nType 1-5 for a computer player.\n1 is the easiest. 5 is the hardest.");
+        player1 = scan.nextInt();
+        while (player1 < 0 || player1 > 5){
+            System.out.println("Please put a number between 0 and 5\n");
+            System.out.println("Choose a difficulty for Player 1.\nType 0 for a human player.\nType 1-5 for a computer player.\n1 is the easiest. 5 is the hardest.");
+            player1 = scan.nextInt();
+        }
+        if(player1 == 0){
+            System.out.println("Player 1 is a human player.\n");
+        }
+        else{
+            System.out.println("Player 1 is an AI of difficulty " + player1 + ".\n");
+        }
+        System.out.println("Choose a difficulty for Player 2.\nType 0 for a human player.\nType 1-5 for a computer player.\n1 is the easiest. 5 is the hardest.");
+        player2 = scan.nextInt();
+        while (player2 < 0 || player2 > 5){
+            System.out.println("Please put a number between 0 and 5\n");
+            System.out.println("Choose a difficulty for Player 2.\nType 0 for a human player.\nType 1-5 for a computer player.\n1 is the easiest. 5 is the hardest.");
+            player2 = scan.nextInt();
+        }
+        if(player2 == 0){
+            System.out.println("Player 2 is a human player.\n");
+        }
+        else{
+            System.out.println("Player 2 is an AI of difficulty " + player2 + ".\n");
+        }
 
         char[][] board =
                 {
