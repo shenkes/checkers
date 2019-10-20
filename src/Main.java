eed024f18f00baea0bee3ebcc3458fba7a09e68d
Sam Henkes - Desktop <shenkes101@yahoo.com>
2019-10-19 22:04:36 -0500

    updated visuals

--- src/Main.java
+++ src/Main.java
@@ -12,15 +12,15 @@ public class Main {
 
         System.out.println("Choose a difficulty for Player 1.\n" +
                 "Type 0 for a human player.\n" +
-                "Type 1-5 for a computer player.\n" +
-                "1 is the easiest. 5 is the hardest.");
+                "Type 1-10 for a computer player.\n" +
+                "1 is the easiest. 10 is the hardest.");
         player1 = scan.nextInt();
-        while (player1 < 0 || player1 > 15) {
-            System.out.println("Please put a number between 0 and 5\n");
+        while (player1 < 0 || player1 > 10) {
+            System.out.println("Please put a number between 0 and 10\n");
             System.out.println("Choose a difficulty for Player 1.\n" +
                     "Type 0 for a human player.\n" +
-                    "Type 1-5 for a computer player.\n" +
-                    "1 is the easiest. 5 is the hardest.");
+                    "Type 1-10 for a computer player.\n" +
+                    "1 is the easiest. 10 is the hardest.");
             player1 = scan.nextInt();
         }
         if (player1 == 0) {
@@ -31,15 +31,15 @@ public class Main {
 
         System.out.println("Choose a difficulty for Player 2.\n" +
                 "Type 0 for a human player.\n" +
-                "Type 1-5 for a computer player.\n" +
-                "1 is the easiest. 5 is the hardest.");
+                "Type 1-10 for a computer player.\n" +
+                "1 is the easiest. 10 is the hardest.");
         player2 = scan.nextInt();
-        while (player2 < 0 || player2 > 15) {
-            System.out.println("Please put a number between 0 and 5\n");
+        while (player2 < 0 || player2 > 10) {
+            System.out.println("Please put a number between 0 and 10\n");
             System.out.println("Choose a difficulty for Player 2.\n" +
                     "Type 0 for a human player.\n" +
-                    "Type 1-5 for a computer player.\n" +
-                    "1 is the easiest. 5 is the hardest.");
+                    "Type 1-10 for a computer player.\n" +
+                    "1 is the easiest. 10 is the hardest.");
             player2 = scan.nextInt();
         }
         if (player2 == 0) {
@@ -53,7 +53,7 @@ public class Main {
                 "\nType 1 for player 1 or 2 for player 2.");
         first = scan.nextInt();
         while (first < 0 || first > 2) {
-            System.out.println("Please put a number between 0 and 5\n");
+            System.out.println("Please put a number between 0 and 10\n");
             System.out.println("Choose who starts first.\n" +
                     "Type 0 for random.\n" +
                     "Type 1 for player 1 or 2 for player 2.");
